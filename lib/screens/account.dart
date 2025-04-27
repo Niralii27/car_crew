1d5cc45801615e983cdfbd0f1f7070ad457250f0
Niralii27 <akbarinirali27@gmail.com>
2025-04-27 20:56:17 +0530

    User Profile Update Successfully

--- lib/screens/account.dart
+++ lib/screens/account.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -9,17 +14,123 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   bool isPasswordVisible = false;
+  File? _image;
 
-  TextEditingController nameController =
-      TextEditingController(text: 'Felicia Lopez');
-  TextEditingController emailController =
-      TextEditingController(text: 'felicia.lopez@email.com');
-  TextEditingController addressController =
-      TextEditingController(text: '1234 Street Name, City, Country');
-  TextEditingController cityController =
-      TextEditingController(text: 'New York');
-  TextEditingController passwordController =
-      TextEditingController(text: '123456789');
+  final ImagePicker _picker = ImagePicker();
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController cityController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  String? _profileImageUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserData();
+  }
+
+  // Function to pick image from gallery
+  Future<void> _getImageFromGallery() async {
+    final XFile? pickedFile =
+        await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> fetchUserData() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        final userId = user.uid;
+        print("Fetching data for user ID: $userId");
+
+        DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection('UsersTbl')
+            .doc(userId)
+            .get();
+
+        if (userDoc.exists) {
+          print("Document found for user: $userId");
+
+          setState(() {
+            nameController.text = userDoc['UserName'] ?? '';
+            emailController.text = userDoc['UserEmail'] ?? '';
+            addressController.text = userDoc['address'] ?? '';
+            cityController.text = userDoc['city'] ?? '';
+            phoneController.text = userDoc['phone'] ?? '';
+            _profileImageUrl = userDoc['UserImage']; // Get the stored image URL
+          });
+        } else {
+          print("User document not found for ID: $userId");
+        }
+      } else {
+        print("User is not logged in");
+      }
+    } catch (e) {
+      print('Error fetching user data: $e');
+    }
+  }
+
+  Future<String> uploadImageToCloud() async {
+    if (_image == null) return '';
+
+    try {
+      // Initialize the Cloudinary instance with your cloud name and upload preset
+      final cloudinary = CloudinaryPublic('dd8fkpbnm', 'car_Crew_images');
+
+      // Upload file
+      CloudinaryResponse response =
+          await cloudinary.uploadFile(CloudinaryFile.fromFile(
+        _image!.path,
+        folder:
+            'user_profiles', // Optional folder name in your Cloudinary account
+      ));
+
+      // Get the secure URL of the uploaded image
+      return response.secureUrl;
+    } catch (e) {
+      print('Error uploading image to Cloudinary: $e');
+      return '';
+    }
+  }
+
+  Future<void> saveProfile() async {
+    try {
+      final user = FirebaseAuth.instance.currentUser;
+      if (user != null) {
+        final userId = user.uid;
+        print("Saving profile for user ID: $userId");
+
+        String UserImage = await uploadImageToCloud();
+
+        await FirebaseFirestore.instance
+            .collection('UsersTbl')
+            .doc(userId)
+            .update({
+          'UserName': nameController.text,
+          'UserEmail': emailController.text,
+          'address': addressController.text,
+          'city': cityController.text,
+          'phoneNo': phoneController.text,
+          if (UserImage.isNotEmpty)
+            'UserImage': UserImage, // Store Cloudinary URL in 'UserImage' field
+        });
+        // Optionally, add a 'phone' field if you want the user to update it.
+        print("Profile updated successfully with image URL: $UserImage");
+
+        print("Profile updated successfully!");
+      }
+    } catch (e) {
+      print('Error saving profile: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,9 +144,32 @@ class _AccountPageState extends State<AccountPage> {
         child: ListView(
           children: [
             Center(
-              child: CircleAvatar(
-                radius: 70,
-                backgroundImage: AssetImage('assets/user.jpg'),
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 70,
+                    backgroundImage: _image != null
+                        ? FileImage(_image!) as ImageProvider
+                        : _profileImageUrl != null &&
+                                _profileImageUrl!.isNotEmpty
+                            ? NetworkImage(_profileImageUrl!)
+                            : const AssetImage('assets/user.jpg'),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: IconButton(
+                        icon: const Icon(Icons.camera_alt, color: Colors.white),
+                        onPressed: _getImageFromGallery,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
             const SizedBox(height: 20),
@@ -115,34 +249,25 @@ class _AccountPageState extends State<AccountPage> {
               ),
             ),
             const SizedBox(height: 10),
-            const Text('Password:',
+            const Text('Phone No:',
                 style: TextStyle(fontSize: 18, color: Colors.grey)),
             const SizedBox(height: 5),
             TextField(
-              controller: passwordController,
-              obscureText: !isPasswordVisible,
+              controller:
+                  phoneController, // Add phoneController for the phone number
               decoration: InputDecoration(
-                hintText: 'Enter your password',
-                prefixIcon: Icon(Icons.lock, color: Colors.blueAccent),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    isPasswordVisible ? Icons.visibility_off : Icons.visibility,
-                    color: Colors.blueAccent,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      isPasswordVisible = !isPasswordVisible;
-                    });
-                  },
-                ),
+                hintText: 'Enter your phone number',
+                prefixIcon: Icon(Icons.phone, color: Colors.blueAccent),
                 border:
                     OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                 focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blueAccent)),
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                ),
                 enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blueAccent)),
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(color: Colors.blueAccent),
+                ),
               ),
             ),
             const SizedBox(height: 20),
@@ -150,8 +275,29 @@ class _AccountPageState extends State<AccountPage> {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      // Save logic here
+                    onPressed: () async {
+                      await saveProfile();
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Row(
+                            children: [
+                              Icon(Icons.check_circle, color: Colors.white),
+                              SizedBox(width: 10),
+                              Expanded(
+                                  child: Text('Profile updated successfully!')),
+                            ],
+                          ),
+                          backgroundColor: Colors.blue.shade600,
+                          behavior: SnackBarBehavior.floating,
+                          margin: EdgeInsets.symmetric(
+                              horizontal: 20, vertical: 10),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          duration: Duration(seconds: 2),
+                          elevation: 8,
+                        ),
+                      );
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blueAccent,
